08a9ee412f87187d49ec0bb3eb76318bfc14109d
SSSSSea6 <sssssea666@gmail.com>
2025-09-20 16:22:17 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -39,37 +39,23 @@ const handleScanned = async () => {
 };
 </script>
 <template>
-  <div class="bg-white-main min-h-screen flex flex-col items-center justify-center relative p-4">
-    <!-- 左上 logo -->
-    < img src="~/assets/logo.png" class="absolute top-4 left-4 h-12 w-auto" alt="My Logo" />
-
-    <!-- 右上 GitHub 图标 -->
-    <div class="absolute top-4 right-4 flex space-x-4">
-      <a href=" " target="_blank">
-        <VIcon icon="mdi-github" size="large" class="text-gray-800 hover:text-black transition-colors duration-300" />
-      </a >
-      <a href="https://github.com/BeiyanYunyi/totoro-paradise" target="_blank">
-        <VIcon icon="mdi-github" size="large" class="text-gray-800 hover:text-black transition-colors duration-300" />
-      </a >
-    </div>
-
-    <!-- 提醒语句 -->
-    <p class="text-body-1 text-gray-600 mb-4">本项目fork自BeiyanYunyi，我在原项目上稍作一些修改。</p >
-
-    <!-- 简洁内容 -->
-    <p class="text-body-1 mb-4">请用微信扫码登录后点击“下一步”，等待几秒。</p >
-    <VCard :height="200" :width="200" class="border-smooth">
-      < img v-if="!message" :src="data!.imgUrl" class="w-full h-full object-cover" referrerpolicy="no-referrer" />
-      <div v-else class="h-full w-full flex items-center justify-center text-gray-800">
+  <p class="text-subtitle-1">
+    古典时代的人发现人体是权力的对象和目标。……这种人体是被操纵、被塑造、被规训的。它服从，配合，变得灵巧、强壮。“人是机器”这部大书是在两个领域被同时撰写的。
+  </p>
+  <p class="mt-2 text-end">—— 米歇尔·福柯《规训与惩罚》</p>
+  <VDivider class="my-4" />
+  <div class="flex flex-col gap-4">
+    <p class="text-body-1">请用微信扫码，扫码后点击“下一步”按钮</p>
+    <VCard :height="200" :width="200">
+      <img v-if="!message" :src="data!.imgUrl" class="w-100" referrerpolicy="no-referrer" />
+      <div v-else class="h-100 w-100 flex items-center justify-center">
         {{ message }}
       </div>
     </VCard>
-    <div class="mt-4">
+    <div class="mt-2 flex">
       <VBtn color="primary" append-icon="i-mdi-arrow-right" @click="handleScanned"> 下一步 </VBtn>
     </div>
-
-    <!-- 保留诗句：最下方醒目、粗体、大字体 -->
-    <div class="absolute bottom-4 left-0 right-0 text-center text-gray-700 font-bold text-xl pre-wrap">
+    <div class="text-sm pre-wrap">
       {{ poem[Math.floor(Math.random() * poem.length)].join('\n') }}
     </div>
   </div>
